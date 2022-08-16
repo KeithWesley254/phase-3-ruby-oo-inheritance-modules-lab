f2d64d84ab5350f0b26037e132496d19ca9361f2
Keith <keithwesley187@gmail.com>
2022-08-16 18:31:41 +0300

    Done

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -11,10 +11,7 @@ module Memorable
   
     module InstanceMethods
         def initialize
-            
-            def initialize
-                self.class.all << self
-            end
+            self.class.all << self
         end
     end
 end
